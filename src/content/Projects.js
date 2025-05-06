f5674542c80e01ba4b56447c4235cda06b867cf9
Ilsen <romeroilsen01@gmail.com>
2025-05-05 22:18:41 -0400

    Projects listo

---
+++ src/content/Projects.js
@@ -0,0 +1,34 @@
+export const Projects = [
+    {
+        image: "/img/p1.jpeg",
+        name : "Gestión de fichas técnicas de mantenimiento de computadoras",
+        description : "Es una aplicación web para los laboratorios de computo, donde se registra los mantenimientos preventivos, correctivos y lógicos, además almacena las características y componentes de las computadoras.",
+        technologies : ['react', 'node', 'mongo db'],
+        page : "https://github.com/romero102/inf-prototipo-dev",
+        github : "https://github.com/romero102/inf-prototipo-dev"
+    },
+    {
+        image: "/img/p2.jpeg",
+        name : "Gestión de fichas técnicas de mantenimiento de computadoras",
+        description : "Es una aplicación web para los laboratorios de computo, donde se registra los mantenimientos preventivos, correctivos y lógicos, además almacena las características y componentes de las computadoras.",
+        technologies : ['react', 'node', 'mongo db'],
+        page : "https://github.com/romero102/inf-prototipo-dev",
+        github : "https://github.com/romero102/inf-prototipo-dev"
+    },
+    {
+        image: "/img/p3.jpeg",
+        name : "Gestión de fichas técnicas de mantenimiento de computadoras",
+        description : "Es una aplicación web para los laboratorios de computo, donde se registra los mantenimientos preventivos, correctivos y lógicos, además almacena las características y componentes de las computadoras.",
+        technologies : ['react', 'node', 'mongo db'],
+        page : "",
+        github : "https://github.com/romero102/inf-prototipo-dev"
+    },
+    {
+        image: "/img/p4.jpeg",
+        name : "Gestión de fichas técnicas de mantenimiento de computadoras",
+        description : "Es una aplicación web para los laboratorios de computo, donde se registra los mantenimientos preventivos, correctivos y lógicos, además almacena las características y componentes de las computadoras.",
+        technologies : ['react', 'node', 'mongo db'],
+        page : "https://github.com/romero102/inf-prototipo-dev",
+        github : "https://github.com/romero102/inf-prototipo-dev"
+    }
+]
