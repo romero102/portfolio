6fda99b1f39ff4ae7cc4e1ecb3357fbaf709d233
Ilsen <romeroilsen01@gmail.com>
2026-01-04 14:52:14 -0400

    End

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,4 +15,6 @@ export default defineConfig({
   },
 
   integrations: [icon()],
+  site: 'https://romero102.github.io',
+  base: 'portfolio'
 });
--- src/content/Projects.js
+++ src/content/Projects.js
@@ -1,34 +1,32 @@
 export const Projects = [
     {
-        image: "/img/p1.jpeg",
+        image: "/img/p-1-.png",
+        name : "WhatsApp Bot",
+        description : "Es una aplicación web de un bot de WhatsApp para el envío masivo de mensajes.",
+        technologies : ['react', 'node', 'mongo db'],
+        page : "https://whatsapp-astro.vercel.app/",
+        github : "https://github.com/R1mber/whatsapp-astro"
+    },
+    {
+        image: "/img/p-2-.png",
+        name : "Unidad educativa",
+        description : "Es una aplicación web para una unidad educativa.",
+        technologies : ['react', 'node', 'mongo db'],
+        page : "https://titi-db000.web.app",
+        github : "https://gitlab.com/rimber.re95/titi"
+    },
+    {
+        image: "/img/p-3-.png",
         name : "Gestión de fichas técnicas de mantenimiento de computadoras",
         description : "Es una aplicación web para los laboratorios de computo, donde se registra los mantenimientos preventivos, correctivos y lógicos, además almacena las características y componentes de las computadoras.",
         technologies : ['react', 'node', 'mongo db'],
-        page : "https://github.com/romero102/inf-prototipo-dev",
         github : "https://github.com/romero102/inf-prototipo-dev"
     },
     {
-        image: "/img/p2.jpeg",
-        name : "Gestión de fichas técnicas de mantenimiento de computadoras",
-        description : "Es una aplicación web para los laboratorios de computo, donde se registra los mantenimientos preventivos, correctivos y lógicos, además almacena las características y componentes de las computadoras.",
-        technologies : ['react', 'node', 'mongo db'],
-        page : "https://github.com/romero102/inf-prototipo-dev",
-        github : "https://github.com/romero102/inf-prototipo-dev"
-    },
-    {
-        image: "/img/p3.jpeg",
-        name : "Gestión de fichas técnicas de mantenimiento de computadoras",
-        description : "Es una aplicación web para los laboratorios de computo, donde se registra los mantenimientos preventivos, correctivos y lógicos, además almacena las características y componentes de las computadoras.",
-        technologies : ['react', 'node', 'mongo db'],
-        page : "",
-        github : "https://github.com/romero102/inf-prototipo-dev"
-    },
-    {
-        image: "/img/p4.jpeg",
-        name : "Gestión de fichas técnicas de mantenimiento de computadoras",
-        description : "Es una aplicación web para los laboratorios de computo, donde se registra los mantenimientos preventivos, correctivos y lógicos, además almacena las características y componentes de las computadoras.",
-        technologies : ['react', 'node', 'mongo db'],
-        page : "https://github.com/romero102/inf-prototipo-dev",
-        github : "https://github.com/romero102/inf-prototipo-dev"
+        image: "/img/p-4-.png",
+        name : "Almacen",
+        description : "Es una aplicación web para gestionar un almacen.",
+        technologies : ['node', 'mongo db'],
+        github : "https://github.com/romero102/Backend-almacen"
     }
 ]
